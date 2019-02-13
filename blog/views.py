47f7dd21661862ebd9ea56792ceab85a23f6379a
RafelQC <rquetglas@apsl.net>
2019-02-13 16:34:00 +0100

    Added new view post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import now
 from .models import Post
 
@@ -7,3 +7,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    detail = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'detail': detail})
